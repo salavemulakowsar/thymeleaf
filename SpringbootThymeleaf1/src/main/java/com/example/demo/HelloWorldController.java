e022e41cea4230deade8bdc5a9d0cf52e80880db
kowsar <salavemulak@gmail.com>
2021-05-04 11:15:52 +0530

    add-css-js-demo

--- SpringbootThymeleaf1/src/main/java/com/example/demo/HelloWorldController.java
+++ SpringbootThymeleaf1/src/main/java/com/example/demo/HelloWorldController.java
@@ -10,6 +10,6 @@ public class HelloWorldController {
     @GetMapping("/hello")
     public String hello(Model model) {
         model.addAttribute("message", "Hello World!");
-        return "helloworld";
+        return "add-css-js-demo";
     }
 }
